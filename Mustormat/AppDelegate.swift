aa2ffefa36771024550c52ea159d52a94d618a21
Kamil Abdulatipov <terapit1@gmail.com>
2017-11-06 13:51:21 +0300

    Initial Commit

---
+++ Mustormat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mustormat
+//
+//  Created by Kamil on 06/11/2017.
+//  Copyright © 2017 Kamil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mustormat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mustormat
+//
+//  Created by Kamil on 06/11/2017.
+//  Copyright © 2017 Kamil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
